6adc7c6f89144d902d2b44e4e85a6d646d9947ee
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-10 12:27:36 +0530

    DB Connection setup

--- website/src/app/api/hello/route.ts
+++ website/src/app/api/hello/route.ts
@@ -1,4 +1,19 @@
 import { NextResponse } from "next/server";
+import MongoConnection from "../_database/database";
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  console.log("Define the MONGODB_URI environment variable inside .env");
+  process.exit(1);
+}
+
+const mongoConnection = new MongoConnection(uri);
+mongoConnection.connect(() => {
+  console.log("Connected to MongoDB");
+});
 
 export async function GET() {
   return NextResponse.json({ message: "Hello World" });
