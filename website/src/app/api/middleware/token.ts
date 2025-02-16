83d43af45a81837b168a2d4c04909857cb8f101e
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-02-17 00:07:20 +0530

    feat:Adding middleware for protected routes and token generation

--- website/src/app/api/middleware/token.ts
+++ website/src/app/api/middleware/token.ts
@@ -1,5 +1,8 @@
+"use server";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 dotenv.config();
 
@@ -10,15 +13,41 @@ interface Payload {
   password: string;
 }
 
-function generateToken(email: string, password: string): string {
+async function generateToken(
+  id: string,
+  email: string,
+  password: string
+): string {
+
   const payload: Payload = {
+    _id: id,
     email: email,
     password: password,
   };
 
-  const token = jwt.sign(payload, secretKey, { expiresIn: "30d" });
+  console.log((await cookies()).get("token")?.value);
+  const allCookies = (await cookies()).getAll();
+  console.log(allCookies);
 
-  return token;
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const token = jwt.sign(payload, secretKey, { expiresIn: "7d" });
+  const cookieStore = await cookies();
+
+  cookieStore.set("token", token, {
+    httpOnly: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
+    secure: false,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+
+  // console.log((await cookies()).get("token")?.value);
+  // const allCookies = (await cookies()).getAll();
+  // console.log("All cookies:", allCookies);
+
+  // console.log("cookie set");
+  // return token;
 }
 
 export default generateToken;
