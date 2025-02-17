cbb9b4422eec0fdfc832ab2056c0665b33f27303
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-02-17 21:27:54 +0530

    fix:Seems can't use mongoose when running in edge server so removing it

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import validateToken from "./app/api/middleware/validate";
+import validateToken from "./app/api/services/validate";
 
 const protectedRoutes = ["/api/dashboard/info", "/api/logout"];
 const publicRoutes = ["/api/login", "/api/info"];
@@ -16,16 +16,16 @@ interface Decodedcookie {
 
 export default async function middleware(req: NextRequest, res: NextResponse) {
   const path = req.nextUrl.pathname;
-  console.log(path);
+  //   console.log(path);
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
 
   const cookie = (await cookies()).get("token")?.value;
+  // console.log((await cookies()).getAll());
   //   console.log("Token cookie:", cookie);
 
-  //   console.log(await validateToken(cookie));
-  if (isProtectedRoute && !(await validateToken(cookie))) {
-    console.log("yes");
+  if (isProtectedRoute && (!cookie || cookie == undefined)) {
+    // console.log("yes");
 
     //TODO if u create a login section it will directly redirect to login page
     return NextResponse.redirect(new URL("/login", req.nextUrl));
